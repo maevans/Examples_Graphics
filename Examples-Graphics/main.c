589b01e169b78e32ae08ff1a4e791cca595c7dd5
Mahalia E <maev8907@colorado.edu>
2020-09-17 20:12:34 -0600

    Initial Commit

---
+++ Examples-Graphics/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Examples-Graphics
+//
+//  Created by Mahalia Evans on 9/17/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
